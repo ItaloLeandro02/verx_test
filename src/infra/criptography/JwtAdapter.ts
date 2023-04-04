b919fe2813d6a087ccf9f0f306efe8dc9d8249e8
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 12:50:27 -0300

    feat: deve chamar Verify com os dados corretos

--- src/infra/criptography/JwtAdapter.ts
+++ src/infra/criptography/JwtAdapter.ts
@@ -1,13 +1,17 @@
 import jwt from 'jsonwebtoken';
-import { Encrypter } from "@/data/protocols/criptography/Encrypter";
+import { Decrypter, Encrypter } from "@/data/protocols/criptography";
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
     constructor(
         private readonly secretKey: string
     ) {}
-
+    
     async encrypt(plainValue: string): Promise<string> {
         const token = jwt.sign({ id: plainValue }, this.secretKey); 
         return Promise.resolve(token);
+    }
+    async decrypt(encryptedText: string): Promise<string> {
+        jwt.verify(encryptedText, this.secretKey);
+        return '';
     }
 }
