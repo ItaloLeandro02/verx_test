b2d2912ec2823dac6dd0a42d120ad74ddf7bbe22
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 14:48:39 -0300

    deve retornar undefined caso HashComparer retorne undefined

--- src/data/test/HashComparerSpy.ts
+++ src/data/test/HashComparerSpy.ts
@@ -2,11 +2,12 @@ import { HashComparer } from "@/data/protocols/criptography";
 
 export class HashComparerSpy implements HashComparer {
     plainTextParam: string = ''; 
-    digestParam: string = ''
+    digestParam: string = '';
+    result: boolean = true;
     
     async compare (plainText: string, digest: string): Promise<boolean> {
         this.plainTextParam = plainText;
         this.digestParam = digest;
-        return Promise.resolve(true);
+        return Promise.resolve(this.result);
     }
 }
