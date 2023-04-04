17d1e0582d8aa6f150c29e7281fa5e1bac6ceda3
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 09:29:18 -0300

    test: deve retornar 400 caso a senha não seja informada

--- test/presentation/controllers/LoginController.spec.ts
+++ test/presentation/controllers/LoginController.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import { LoginController } from "@/presentation/controllers/LoginController";
 import { MissingError } from "@/presentation/errors";
 import { HttpRequest } from "@/presentation/protocols";
@@ -5,11 +6,29 @@ import { HttpRequest } from "@/presentation/protocols";
 describe('LoginController', () => {
     it ('Deve retornar 400 caso o e-mail não seja informado', async () => {
         const sut = new LoginController();
-        const httpRequest: HttpRequest = {};
+        const httpRequest: HttpRequest = {
+            body: {
+                password: faker.internet.password()
+            }
+        };
         const httpResponse = await sut.handle(httpRequest);
         expect(httpResponse).toEqual({
             statusCode: 400,
             body: new MissingError('email')
+        });
+    });
+
+    it ('Deve retornar 400 caso a senha não seja informada', async () => {
+        const sut = new LoginController();
+        const httpRequest: HttpRequest = {
+            body: {
+                email: faker.internet.email()
+            }
+        };
+        const httpResponse = await sut.handle(httpRequest);
+        expect(httpResponse).toEqual({
+            statusCode: 400,
+            body: new MissingError('password')
         });
     });
 });
