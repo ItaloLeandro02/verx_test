9e27a5299eaf94ec238945d148fdca7defd6f56b
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 10:27:12 -0300

    test: deve chamar EmailValidator com os valores corretos

--- test/validation/ validators/EmailValidation.spec.ts
+++ test/validation/ validators/EmailValidation.spec.ts
@@ -2,35 +2,34 @@ import { faker } from "@faker-js/faker";
 import { InvalidParamError } from "@/presentation/errors";
 import { EmailValidator } from "@/validation/protocols";
 import { EmailValidation } from "@/validation/validators";
+import { EmailValidatorSpy } from "@/validation/test";
 
 type SutTypes = {
     sut: EmailValidation,
-    emailValidator: EmailValidator
+    emailValidator: EmailValidatorSpy
 };
 
 const makeSut = (): SutTypes => {
-    const emailValidator = makeEmailValidatorMock();
+    const emailValidator = new EmailValidatorSpy();
     const sut = new EmailValidation('email', emailValidator);
     return {
         sut,
         emailValidator
     };
 };
-const makeEmailValidatorMock = (): EmailValidator => {
-    class EmailValidatorMock implements EmailValidator {
-        emailIsValid: boolean = true;
-        isValid(email: string): boolean {
-            return this.emailIsValid;
-        }
-    }
-    return new EmailValidatorMock();
-};
 
 describe('EmailValidation', () => {
     it ('Deve retornar erro caso EmailValidator retorne false', () => {
-        const { sut } = makeSut();
+        const { sut, emailValidator } = makeSut();
+        emailValidator.emailIsValid = false;
         const input = { email: faker.internet.email() };
         const error = sut.validate(input);
         expect(error).toEqual(new InvalidParamError('email'));
+    });
+    it ('Deve chamar EmailValidator com os valores corretos', () => {
+        const { sut, emailValidator } = makeSut();
+        const input = { email: faker.internet.email() };
+        sut.validate(input);
+        expect(emailValidator.emailParam).toEqual(input.email);
     });
 });
