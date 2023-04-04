df7f6da2ef6a1025ad00f737d0d25513361582aa
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 14:28:29 -0300

    test: deve retornar uma exceção caso LoadAccountByEmailRepository falhe

--- test/data/usecases/account/authentication/DbAuthentication.spec.ts
+++ test/data/usecases/account/authentication/DbAuthentication.spec.ts
@@ -3,15 +3,38 @@ import { AuthenticationParams } from "@/domain/usercases/account";
 import { DbAuthentication } from "@/data/usecases/account/DbAuthentication";
 import { LoadAccountByEmailRepositorySpy } from "@/data/test";
 
+type SutTypes = { 
+    sut: DbAuthentication,
+    loadAccountByEmailRepository: LoadAccountByEmailRepositorySpy
+};
+
+const makeSut = (): SutTypes => {
+    const loadAccountByEmailRepository = new LoadAccountByEmailRepositorySpy(); 
+    const sut = new DbAuthentication(loadAccountByEmailRepository);
+    return {
+        sut,
+        loadAccountByEmailRepository
+    }
+};
+const mockInput = (): AuthenticationParams => ({
+    email: faker.internet.email(),
+    password: faker.internet.password()
+});
+
 describe('DbAuthentication', () => {
-    it ('Deve chamar LoadAccountByEmailRepository com o email correto', async () => {
-        const loadAccountByEmailRepository = new LoadAccountByEmailRepositorySpy(); 
-        const sut = new DbAuthentication(loadAccountByEmailRepository);
-        const input: AuthenticationParams = {
-            email: faker.internet.email(),
-            password: faker.internet.password()
-        };
-        await sut.auth(input);
-        expect(loadAccountByEmailRepository.emailParam).toBe(input.email);
+    describe('LoadAccountByEmailRepository', () => {
+        it ('Deve chamar LoadAccountByEmailRepository com o email correto', async () => {
+            const { sut, loadAccountByEmailRepository } = makeSut();
+            const input = mockInput();
+            await sut.auth(input);
+            expect(loadAccountByEmailRepository.emailParam).toBe(input.email);
+        });
+        it ('Deve retornar uma exceção caso LoadAccountByEmailRepository falhe', async () => {
+            const { sut, loadAccountByEmailRepository } = makeSut();
+            jest.spyOn(loadAccountByEmailRepository, 'loadByEmail').mockImplementationOnce(() => { throw new Error() });
+            const input = mockInput();
+            const promise = sut.auth(input);
+            await expect(promise).rejects.toThrow();
+        });
     });
 });
