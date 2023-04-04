426432a28d8aae20f55de98dc00394ce2db2daed
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 20:49:45 -0300

    test: deve atualizar o campo accessToken

--- test/infra/db/knex/account/AccountKnexRepository.spec.ts
+++ test/infra/db/knex/account/AccountKnexRepository.spec.ts
@@ -21,19 +21,19 @@ const defaultAccount = {
 };
 
 describe('AccountKnexRepository', () => {
-    describe('LoadAccountByEmailRepository', () => {
-        beforeAll(async () => {
-            await KnexHelper.connect();
-            await KnexHelper.runMigrations();
-            await KnexHelper.runSeeders();
-        });
-        afterEach(async () => {
-            await KnexHelper.deleteData('accounts');
-        });
-        afterAll(async () => {
-            await KnexHelper.roolbackMigrations();
-        });
+    beforeAll(async () => {
+        await KnexHelper.connect();
+        await KnexHelper.runMigrations();
+        await KnexHelper.runSeeders();
+    });
+    afterEach(async () => {
+        await KnexHelper.deleteData('accounts');
+    });
+    afterAll(async () => {
+        await KnexHelper.roolbackMigrations();
+    });
 
+    describe('LoadAccountByEmailRepository', () => {
         it ('Deve retornar um account caso exista um registro no banco com o email informado', async () => {
             const { sut } = makeSut();
             const account = await sut.loadByEmail(defaultAccount.email);
@@ -47,6 +47,18 @@ describe('AccountKnexRepository', () => {
             const { sut } = makeSut();
             const account = await sut.loadByEmail(defaultAccount.email);
             expect(account).toBeFalsy();
+        });
+    });
+    describe('UpdateAccessTokenRepository', () => {
+        it ('Deve atualizar o campo accessToken', async () => {
+            await KnexHelper.runSeeders();
+            const { sut } = makeSut();
+            const account = await sut.loadByEmail(defaultAccount.email);
+            const fakeAccount: any = account;
+            await sut.updateAccessToken(fakeAccount.id, 'any_token');
+            const account2: any = await sut.loadByEmail(defaultAccount.email);
+            expect(account2).toBeTruthy();
+            expect(account2.accessToken).toBe('any_token');
         });
     });
 });
