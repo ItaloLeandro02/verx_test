97dc943bad87575946d572c4618de11ff5b51e8c
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 09:29:10 -0300

    feat: deve retornar 400 caso a senha não seja informada

--- src/presentation/controllers/LoginController.ts
+++ src/presentation/controllers/LoginController.ts
@@ -3,9 +3,22 @@ import { MissingError } from "../errors";
 
 export class LoginController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        return {
-            statusCode: 400,
-            body: new MissingError('email')
+        const { email, password } = httpRequest.body;
+        if (!email) {
+            return {
+                statusCode: 400,
+                body: new MissingError('email')
+            }
         }
+        if (!password) {
+            return {
+                statusCode: 400,
+                body: new MissingError('password')
+            }
+        }
+        return {
+            statusCode: 200,
+            body: null
+        };
     }
 }
