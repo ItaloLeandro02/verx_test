1539d08b6a6d34d76494b6a11a552a87b3bfd5c6
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 11:28:35 -0300

    test: deve chamar Authentication com os dados corretos

--- test/presentation/controllers/LoginController.spec.ts
+++ test/presentation/controllers/LoginController.spec.ts
@@ -1,21 +1,24 @@
 import { faker } from '@faker-js/faker';
 import { LoginController } from "@/presentation/controllers/LoginController";
 import { HttpRequest } from "@/presentation/protocols";
-import { ValidationSpy } from '@/presentation/test';
+import { AuthenticationSpy, ValidationSpy } from '@/presentation/test';
 import { badRequest } from '@/presentation/helpers/http';
 import { InvalidParamError } from '@/presentation/errors';
 
 type SutTypes = {
     sut: LoginController,
-    validation: ValidationSpy
+    validation: ValidationSpy,
+    authentication: AuthenticationSpy
 };
 
 const makeSut = (): SutTypes => {
     const validation = new ValidationSpy();
-    const sut = new LoginController(validation);
+    const authentication = new AuthenticationSpy();
+    const sut = new LoginController(validation, authentication);
     return {
         sut,
-        validation
+        validation,
+        authentication
     };
 };
 const makeHttpRequest = (): HttpRequest => {
@@ -28,17 +31,27 @@ const makeHttpRequest = (): HttpRequest => {
 }
 
 describe('LoginController', () => {
-    it ('Deve chamar Validations com os dados corretos', async () => {
-        const { sut, validation } = makeSut();
-        const httpRequest = makeHttpRequest();
-        await sut.handle(httpRequest);
-        expect(validation.inputParams).toEqual(httpRequest.body);
+    describe('Validation', () => {
+        it ('Deve chamar Validations com os dados corretos', async () => {
+            const { sut, validation } = makeSut();
+            const httpRequest = makeHttpRequest();
+            await sut.handle(httpRequest);
+            expect(validation.inputParams).toEqual(httpRequest.body);
+        });
+        it ('Deve retornar 400 caso alguma validação falhe', async () => {
+            const { sut, validation } = makeSut();
+            jest.spyOn(validation, 'validate').mockImplementationOnce(() => new InvalidParamError('email'));
+            const httpRequest = makeHttpRequest();
+            const httpResponse = await sut.handle(httpRequest);
+            expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')));
+        });
     });
-    it ('Deve retornar 400 caso alguma validação falhe', async () => {
-        const { sut, validation } = makeSut();
-        jest.spyOn(validation, 'validate').mockImplementationOnce(() => new InvalidParamError('email'));
-        const httpRequest = makeHttpRequest();
-        const httpResponse = await sut.handle(httpRequest);
-        expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')));
+    describe('Authentication', () => {
+        it ('Deve chamar Authentication com os dados corretos', async () => {
+            const { sut, authentication } = makeSut();
+            const httpRequest = makeHttpRequest();
+            await sut.handle(httpRequest);
+            expect(authentication.authenticationParams).toEqual(httpRequest.body);
+        });
     });
 });
