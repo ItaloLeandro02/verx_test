bc6babbdafdb3b392e5f205787ce0d0a588d22e8
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 09:32:07 -0300

    refactor: método para criação de sut

--- test/presentation/controllers/LoginController.spec.ts
+++ test/presentation/controllers/LoginController.spec.ts
@@ -3,14 +3,30 @@ import { LoginController } from "@/presentation/controllers/LoginController";
 import { MissingError } from "@/presentation/errors";
 import { HttpRequest } from "@/presentation/protocols";
 
+type SutTypes = {
+    sut: LoginController
+};
+
+const makeSut = (): SutTypes => {
+    const sut = new LoginController();
+    return {
+        sut
+    };
+};
+const makeHttpRequest = (): HttpRequest => {
+    return {
+        body: {
+            email: faker.internet.email(),
+            password: faker.internet.password()
+        }
+    }
+}
+
 describe('LoginController', () => {
     it ('Deve retornar 400 caso o e-mail não seja informado', async () => {
-        const sut = new LoginController();
-        const httpRequest: HttpRequest = {
-            body: {
-                password: faker.internet.password()
-            }
-        };
+        const { sut } = makeSut();
+        const httpRequest = makeHttpRequest();
+        delete httpRequest.body.email;
         const httpResponse = await sut.handle(httpRequest);
         expect(httpResponse).toEqual({
             statusCode: 400,
@@ -19,12 +35,9 @@ describe('LoginController', () => {
     });
 
     it ('Deve retornar 400 caso a senha não seja informada', async () => {
-        const sut = new LoginController();
-        const httpRequest: HttpRequest = {
-            body: {
-                email: faker.internet.email()
-            }
-        };
+        const { sut } = makeSut();
+        const httpRequest = makeHttpRequest();
+        delete httpRequest.body.password;
         const httpResponse = await sut.handle(httpRequest);
         expect(httpResponse).toEqual({
             statusCode: 400,
