ea934e73324248e4aa3ebcfd9163568a60e708d7
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 12:36:06 -0300

    test: deve chamar método Sign em JWTAdapter com os valores corretos

---
+++ test/infra/criptography/jwt-adapter/JwtAdapter.spec.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import { faker } from "@faker-js/faker";
+import { JwtAdapter } from "@/infra/criptography/JwtAdapter";
+
+jest.mock('jsonwebtoken', () => ({
+    sign (): Promise<string> {
+        return Promise.resolve('any_token');
+    }
+}));
+
+const secret = faker.random.word();
+
+describe('JwtAdapter', () => {
+    describe('sign()', () => {
+        it ('Deve chamar sign com os valores corretos', async () => {
+            const signSpy = jest.spyOn(jwt, 'sign');
+            const sut = new JwtAdapter(secret);
+            await sut.encrypt('any_value');
+            expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, secret);
+        });
+    });
+});
