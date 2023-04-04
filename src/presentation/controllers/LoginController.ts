e03c07dc4152409e95a842a78a4677301275b674
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 11:08:04 -0300

    feat: deve chamar Validation com os dados corretos

--- src/presentation/controllers/LoginController.ts
+++ src/presentation/controllers/LoginController.ts
@@ -1,15 +1,13 @@
-import { Controller, HttpRequest, HttpResponse } from "@/presentation/protocols";
-import { MissingParamError } from "@/presentation/errors";
+import { Controller, HttpRequest, HttpResponse, Validation } from "@/presentation/protocols";
 import { badRequest } from "@/presentation/helpers/http";
 
 export class LoginController implements Controller {
+    constructor(private readonly validation: Validation) {}
+    
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const { email, password } = httpRequest.body;
-        if (!email) {
-            return badRequest(new MissingParamError('email'));
-        }
-        if (!password) {
-            return badRequest(new MissingParamError('password'));
+        const error = this.validation.validate(httpRequest.body);
+        if (error) {
+            return badRequest(error);
         }
         return {
             statusCode: 200,
