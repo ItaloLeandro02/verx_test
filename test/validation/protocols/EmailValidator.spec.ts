cb561cc5f5546724831f9cb622657d8bd2b273d4
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 10:02:09 -0300

    refactor: método para criação de sut

--- test/validation/protocols/EmailValidator.spec.ts
+++ test/validation/protocols/EmailValidator.spec.ts
@@ -2,9 +2,20 @@ import { faker } from "@faker-js/faker";
 import { InvalidParamError } from "@/presentation/errors";
 import { EmailValidator } from "@/validation/EmailValidator";
 
+type SutTypes = {
+    sut: EmailValidator
+};
+
+const makeSut = (): SutTypes => {
+    const sut = new EmailValidator();
+    return {
+        sut
+    };
+};
+
 describe('EmailValidator', () => {
     it ('Deve retornar erro caso EmailValidator retorne false', () => {
-        const sut = new EmailValidator();
+        const { sut } = makeSut();
         const input = { email: faker.internet.email() };
         const error = sut.validate(input);
         expect(error).toEqual(new InvalidParamError('email'));
