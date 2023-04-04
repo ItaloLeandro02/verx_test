8849831ae0fccff47275431a1eedc3f9aeb9f14e
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 15:34:27 -0300

    feat: deve chamar compare com os valores corretos

---
+++ src/infra/criptography/bcrypt-adapter/BcryptAdapter.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt';
+import { HashComparer } from "@/data/protocols/criptography";
+
+export class BcryptAdapter implements HashComparer {
+    constructor(
+        private readonly salt: number
+    ) {}
+
+    async compare(plainText: string, digest: string): Promise<boolean> {
+        await bcrypt.compare(plainText, digest);
+        return true;    
+    }
+}
