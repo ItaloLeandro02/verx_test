1c959dab88205189fd5c246d6b918cbf52dd3e00
Italo Leandro <italo_leandro@outlook.com>
2023-04-05 12:02:26 -0300

    feat: deve retornar negativo caso cocaína esteja com um valor maior do que o permitido, mas com : Benzoilecgonina, Cocaetileno ou Norcocaína com valores menores que 0.05

--- src/utils/AppCalculateSampleResult.ts
+++ src/utils/AppCalculateSampleResult.ts
@@ -9,13 +9,23 @@ export class AppCalculateSampleResult implements CalculateSampleResult {
             const itemCode = item[0]; 
             const itemCutOffScore = item[1]; 
             const drugValue = sampleAnalyze[itemCode as keyof SampleAnalyzeParams]; 
-            if (typeof drugValue == 'number') {
-                result = drugValue > itemCutOffScore;
-                if (result) {
+            if (typeof drugValue === 'number') {
+                const isAllowed = drugValue > itemCutOffScore;
+                if (isAllowed && (itemCode !== 'Cocaína' || this.verifyCocainaCase(sampleAnalyze))) {
+                    result = isAllowed;
                     break;
                 }
             }
         }
         return result ? "positivo" : "negativo";
+    }
+    private verifyCocainaCase(sampleAnalyze: SampleAnalyzeParams): boolean {
+        const drugsToVerify = ['Benzoilecgonina', 'Cocaetileno', 'Norcocaína'];
+        return drugsToVerify.some(drugCode => {
+            const sampleDrugValue = sampleAnalyze[drugCode as keyof SampleAnalyzeParams]; 
+            if (typeof sampleDrugValue === 'number') {
+                return sampleDrugValue >= 0.05
+            }
+        });
     }
 }
