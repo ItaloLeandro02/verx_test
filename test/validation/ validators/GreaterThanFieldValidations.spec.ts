63ca6766a1582325ee7d920e8cbc46841cfe6f96
Italo Leandro <italo_leandro@outlook.com>
2023-04-05 09:13:06 -0300

    test: deve retornar undefined caso GreaterThanFieldValidation seja valido

--- test/validation/ validators/GreaterThanFieldValidations.spec.ts
+++ test/validation/ validators/GreaterThanFieldValidations.spec.ts
@@ -1,6 +1,6 @@
 import { faker } from "@faker-js/faker";
-import { InvalidParamError, MissingParamError } from "@/presentation/errors";
-import { GreaterThanFieldValidation, RequiredFieldValidation } from "@/validation/validators";
+import { InvalidParamError } from "@/presentation/errors";
+import { GreaterThanFieldValidation } from "@/validation/validators";
 
 type SutTypes = {
     sut: GreaterThanFieldValidation
@@ -17,8 +17,14 @@ const makeSut = (): SutTypes => {
 describe('GreaterThanFieldValidation', () => {
     it ('Deve retornar InvalidParamError caso o campo seja menor do que oito caracteres', () => {
         const { sut } = makeSut();
-        const input = { name: faker.lorem.lines(7) };
+        const input = { field: faker.lorem.word(7) };
         const error = sut.validate(input);
         expect(error).toEqual(new InvalidParamError('field'));
+    });
+    it ('Deve retornar undefined caso o campo seja maior ou igual do que oito caracteres', () => {
+        const { sut } = makeSut();
+        const input = { field: faker.lorem.word(8) };
+        const error = sut.validate(input);
+        expect(error).toBeUndefined();
     });
 });
