b1ee03383fba67ed2a9ddd6a486b63c5b5bc9966
Italo Leandro <italo_leandro@outlook.com>
2023-04-05 16:11:00 -0300

    refactor: remoção de código desnecessário

--- test/data/usecases/account/load-account-by-token/DbLoadAccountByToken.spec.ts
+++ test/data/usecases/account/load-account-by-token/DbLoadAccountByToken.spec.ts
@@ -19,59 +19,50 @@ const makeSut = (): SutTypes => {
         loadAccountByTokenRepository
     }
 };
-const mockInput = () => ({
-    accessToken: faker.database.mongodbObjectId()
-});
+const token = faker.internet.password();
 
 describe('DbLoadAccountByToken', () => {
     describe('Decrypter', () => {
         it ('Deve chamar Decrypter com os valores corretos', async () => {
             const { sut, decrypter } = makeSut();
-            const input = mockInput();
-            await sut.load(input.accessToken);
-            expect(decrypter.encryptedTextParam).toBe(input.accessToken);
+            await sut.load(token);
+            expect(decrypter.encryptedTextParam).toBe(token);
         });
         it ('Deve retornar undefined caso Decrypter retorne undefined', async () => {
             const { sut, decrypter } = makeSut();
             decrypter.decryptedTextResult = undefined;
-            const input = mockInput();
-            const account = await sut.load(input.accessToken);
+            const account = await sut.load(token);
             expect(account).toBeUndefined();
         });
         it ('Deve retornar uma exceção caso Decrypter falhe', async () => {
             const { sut, decrypter } = makeSut();
             jest.spyOn(decrypter, 'decrypt').mockImplementationOnce(() => { throw new Error() });
-            const input = mockInput();
-            const promise = sut.load(input.accessToken);
+            const promise = sut.load(token);
             await expect(promise).rejects.toThrowError(new Error());
         });
     });
     describe('LoadAccountByTokenRepository', () => {
         it ('Deve chamar LoadAccountByTokenRepository com os valores corretos', async () => {
             const { sut, loadAccountByTokenRepository, decrypter } = makeSut();
-            const input = mockInput();
-            await sut.load(input.accessToken);
+            await sut.load(token);
             expect(loadAccountByTokenRepository.tokenParam).toBe(decrypter.decryptedTextResult);
         });
         it ('Deve retornar undefined caso LoadAccountByTokenRepository retorne undefined', async () => {
             const { sut, loadAccountByTokenRepository } = makeSut();
             loadAccountByTokenRepository.mockAccount = undefined;
-            const input = mockInput();
-            const account = await sut.load(input.accessToken);
+            const account = await sut.load(token);
             expect(account).toBeUndefined();
         });
         it ('Deve retornar uma exceção caso LoadAccountByTokenRepository falhe', async () => {
             const { sut, loadAccountByTokenRepository } = makeSut();
             jest.spyOn(loadAccountByTokenRepository, 'loadBytoken').mockImplementationOnce(() => { throw new Error() });
-            const input = mockInput();
-            const promise = sut.load(input.accessToken);
+            const promise = sut.load(token);
             await expect(promise).rejects.toThrowError(new Error());
         });
     });
     it ('Deve retornar um account em caso de sucesso', async () => {
         const { sut, loadAccountByTokenRepository, decrypter } = makeSut();
-        const input = mockInput();
-        const account = await sut.load(input.accessToken);
+        const account = await sut.load(token);
         expect(account).toEqual(loadAccountByTokenRepository.mockAccount);
         expect(loadAccountByTokenRepository.tokenParam).toEqual(decrypter.decryptedTextResult);
     });
