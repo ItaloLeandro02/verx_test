23eaef3cd521b9d53342eaa154073b8eab8db342
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 23:30:18 -0300

    feat: retorna o content type como json por padrão

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
-import { bodyParser } from "@/main/middlewares";
+import { bodyParser, contentType } from "@/main/middlewares";
 
 export default (app: Express): void => {
     app.use(bodyParser);
+    app.use(contentType);
 }
--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -1,1 +1,2 @@
 export * from "./BodyParser";
+export * from "./ContentType";
