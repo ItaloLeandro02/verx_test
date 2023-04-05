bf1098dc31aa880c2c9fb456a4bb45860cbd7078
Italo Leandro <italo_leandro@outlook.com>
2023-04-05 00:35:08 -0300

    test: deve retornar 401 caso as credenciais não sejam válidas

---
+++ test/main/routes/LoginRouter.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+import { KnexHelper } from '@/infra/db/knex/helper/KnexHelper';
+
+const defaultAccount = {
+    id: 1,
+    name: 'Verx',
+    email: 'verx@mail.com.br',
+    password: '123345'
+};
+
+describe('Login Routes', () => {
+    beforeAll(async () => {
+        await KnexHelper.connect();
+        await KnexHelper.runMigrations();
+        await KnexHelper.runSeeders();
+    });
+    afterEach(async () => {
+        await KnexHelper.deleteData('accounts');
+    });
+    afterAll(async () => {
+        await KnexHelper.roolbackMigrations();
+    });
+
+    describe('POST / login', () => {
+        it ('Deve retornar 401 caso as credencias não sejam válidas', async () => {
+            await request(app)
+            .post('/api/login')
+            .send({
+                email: defaultAccount.email,
+                password: defaultAccount.password
+            })
+            .expect(401)
+        });
+    });
+});
