59b2616b57b73771a743c074ae5574bb8a6050c5
Italo Leandro <italo_leandro@outlook.com>
2023-04-05 15:47:30 -0300

    test: deve retornar uma exceção caso Decrypter falhe

--- test/data/usecases/account/load-account-by-token/DbLoadAccountByToken.spec.ts
+++ test/data/usecases/account/load-account-by-token/DbLoadAccountByToken.spec.ts
@@ -34,5 +34,12 @@ describe('DbLoadAccountByToken', () => {
             const account = await sut.load(input.accessToken);
             expect(account).toBeUndefined();
         });
+        it ('Deve retornar uma exceção caso Decrypter falhe', async () => {
+            const { sut, decrypter } = makeSut();
+            jest.spyOn(decrypter, 'decrypt').mockImplementationOnce(() => { throw new Error() });
+            const input = mockInput();
+            const promise = sut.load(input.accessToken);
+            await expect(promise).rejects.toThrowError(new Error());
+        });
     });
 });
