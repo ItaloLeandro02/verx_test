bc5a62e1d4ffca4b61ff4083bb6e19948df0d73d
Italo Leandro <italo_leandro@outlook.com>
2023-04-05 11:52:32 -0300

    feat: deve retornar positivo caso algum droga seja maior do que a nota de corte

--- src/utils/AppCalculateSampleResult.ts
+++ src/utils/AppCalculateSampleResult.ts
@@ -3,6 +3,19 @@ import { CalculateSampleResult, SampleAnalyzeParams } from "@/domain/usercases/s
 
 export class AppCalculateSampleResult implements CalculateSampleResult {
     calculate(sampleAnalyze: SampleAnalyzeParams, sampleCutOff: SampleCutOffScore): "positivo" | "negativo" {
-        return "negativo";
+        let result = false;
+        const itemsCutOff = Object.entries(sampleCutOff);
+        for (const item of itemsCutOff) {
+            const itemCode = item[0]; 
+            const itemCutOffScore = item[1]; 
+            const drugValue = sampleAnalyze[itemCode as keyof SampleAnalyzeParams]; 
+            if (typeof drugValue == 'number') {
+                result = drugValue > itemCutOffScore;
+                if (result) {
+                    break;
+                }
+            }
+        }
+        return result ? "positivo" : "negativo";
     }
 }
