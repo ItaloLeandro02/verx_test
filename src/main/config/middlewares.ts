c802510f7ea102be4018095d455a2f3a49a54c23
Italo Leandro <italo_leandro@outlook.com>
2023-04-04 23:39:56 -0300

    feat: desabilita o cache

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
 import { Express } from "express";
-import { bodyParser, contentType, cors } from "@/main/middlewares";
+import { bodyParser, contentType, cors, noCache } from "@/main/middlewares";
 
 export default (app: Express): void => {
     app.use(bodyParser);
     app.use(contentType);
     app.use(cors);
+    app.use(noCache);
 }
