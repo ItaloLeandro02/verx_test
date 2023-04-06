ee23a95abe44e9ce69cc13c77269c2eb0d9c744b
Italo Leandro <italo_leandro@outlook.com>
2023-04-06 09:54:33 -0300

    fix: correção na tipagem

--- tests/data/usecases/sample/DbSampleAnalysis.spec.ts
+++ tests/data/usecases/sample/DbSampleAnalysis.spec.ts
@@ -23,18 +23,18 @@ const makeSut = (): SutTypes => {
 };
 const mockInput = (): SampleAnalyzeParams => ({
     codigoAmostra: "02383322",
-    cocaina: 0.678,
-    anfetamina: 0.1,
-    metanfetamina: 0.1,
-    mda: 0.1,
-    mdma: 0,
-    thc: 0.1,
-    morfina: 0.1,
-    codeina: 0.1,
-    heroina: 0.1,
-    benzoilecgonina: 0,
-    cocaetileno: 0,
-    norcocaina: 0
+    cocaina: "0.678",
+    anfetamina: "0.1",
+    metanfetamina: "0.1",
+    mda: "0.1",
+    mdma: "0",
+    thc: "0.1",
+    morfina: "0.1",
+    codeina: "0.1",
+    heroina: "0.1",
+    benzoilecgonina: "0",
+    cocaetileno: "0",
+    norcocaina: "0"
 });
 
 describe('DbSampleAnalysis', () => {
