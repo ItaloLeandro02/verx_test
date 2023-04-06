4ad7e21c13ef88623c15f0b735d8de71c73925a8
Italo Leandro <italo_leandro@outlook.com>
2023-04-06 09:50:53 -0300

    fix: correção nos inputs recebidos

--- src/domain/usercases/sample/SampleAnalysis.ts
+++ src/domain/usercases/sample/SampleAnalysis.ts
@@ -1,17 +1,17 @@
 export type SampleAnalyzeParams = {
     codigoAmostra: string,
-    cocaina: number,
-    anfetamina: number,
-    metanfetamina: number,
-    mda: number,
-    mdma: number,
-    thc: number,
-    morfina: number,
-    codeina: number,
-    heroina: number,
-    benzoilecgonina: number,
-    cocaetileno: number,
-    norcocaina: number
+    cocaina: string,
+    anfetamina: string,
+    metanfetamina: string,
+    mda: string,
+    mdma: string,
+    thc: string,
+    morfina: string,
+    codeina: string,
+    heroina: string,
+    benzoilecgonina: string,
+    cocaetileno: string,
+    norcocaina: string
 };
 export type SampleAnalyzeResult = {
     codigoAmostra: string,
